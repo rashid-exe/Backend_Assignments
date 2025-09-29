f98ef4a11d2e3203b17906b5b0de44ac5de5adc6
Mr.White <97554758+rashid-exe@users.noreply.github.com>
2025-09-29 12:56:05 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var app = express();
+
+app.set('view engine','pug')
+app.set('views', './views')
+
+const products = ['Watch', 'Oven', 'Microwave', 'Phone', 'tablet', 'Laptop', 'pen drive']
+const clothing = ['Jeans', 'Tshirts', 'Coats', 'Jackets', 'caps', 'mufler']
+
+app.get('/index', (req,res)=>{
+  res.render('index')
+})
+
+app.get('/product', (req,res)=>{
+    res.render('sample', { 'title': 'Electronics', 'message': 'Buy Products', 'prod': products})
+  })
+
+app.get('/clothing', (req,res)=>{
+    res.render('sample', { 'title': 'Clothing', 'message': 'Buy Clothing Products', 'prod': clothing})
+  })
+
+app.listen(8080, ()=>{
+    console.log(' The server has started on port no 8080..')
+})
+
+
+
+
