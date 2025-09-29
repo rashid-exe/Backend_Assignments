d1fdb5244725f936e3b36808f860638f1e096fd0
Mr.White <97554758+rashid-exe@users.noreply.github.com>
2025-09-29 12:57:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,36 +2,41 @@ const express = require("express");
 const {join} = require("path");
 const app = express();
 
-const morgan = require("morgan");
-const {createWriteStream} = require("fs");
- 
-const logFile = join(__dirname, "blogchefNew.log");   // create the log file in the current project directory
- 
-/*--------------Morgan module ------------------*/
-app.use(morgan(":method - :url - :date - :response-time ms"));  // morgan template
-app.use(
-  morgan(":method -:url - :date - :response-time ms", {
-    stream: createWriteStream(logFile, { flags: "a" }),
-  })
-);
-/*--------------Morgan module ------------------*/
-app.use(morgan(":method - :url - :date - :response-time ms"));  // morgan template
-app.use(
-  morgan(":method -:url - :date - :response-time ms", {
-    stream: createWriteStream(logFile, { flags: "a" }),
-  })
-);
-
-
+app.use("/assets", express.static(join(__dirname, "public")));
 app.set("view engine", "pug");
-app.set("views", join(__dirname, "views"));
 
-app
-  .get("/admin/register", (req, res) => res.render("register"))
-
-app.get("/admin/welcome", (req, res) => {
-  res.render("welcome");
+app.get("/", (req, res) => {
+  res.send("<h1>BlogChef</h1>");
 });
 
+app
+  .get("/admin/login", (req, res) => res.render("login"))
+  .post("/admin/login", (req, res) => res.redirect("/admin/dashboard"));
+  
+
+app.get("/admin/dashboard", (req, res) =>
+  res.render("dashboard", {
+    user: "Joe Mockery",
+    posts: [
+      {
+        id: 1,
+        author: "Joe M",
+        title: "I love Express",
+        content: "Express is a wonderful framework for building Node.js apps",
+      },
+      {
+        id: 2,
+        author: "Mike F",
+        title: "Have you tried Pug?",
+        content:
+          "I recently tried the Pug templating language and its excellent",
+      },
+    ],
+  })
+);
+
+app.get("/admin/logout", (req, res) => res.redirect("/admin/login"));
+
+app.post("/admin/approve", (req, res) => res.redirect("/admin/dashboard"));
 
 app.listen(3001, () => console.log("Blog Chef is cooking on port 3001"));
